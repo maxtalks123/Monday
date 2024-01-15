e818b735e601f7daf3ad7efd14301b33481ade34
Max Emerson <maxemerson2013@gmail.com>
2024-01-15 15:51:13 +0000

    finished the exercises

---
+++ Loops/loops.js
@@ -0,0 +1,49 @@
+console.log("hello, world");
+for (let i = 0; i < 5; i++) {
+  console.log("iteration", i);
+}
+
+let counter = 1;
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+
+const foods = [
+  "burger",
+  "pizza",
+  "beef wellington",
+  "curry",
+  "chip shop",
+  "Korean Fried Chicken",
+];
+for (let x = 0; x < foods.length; x++) {
+  console.log(foods[x]);
+}
+
+const animals = [
+  "sloth",
+  "tiger",
+  "capybara",
+  "lion",
+  "elephant",
+  "peregrine falcon",
+];
+for (let y = 0; y < animals.length; y++) {
+  console.log(animals[y]);
+}
+
+const favColors = ["green", "blue", "red", "gold"];
+for (let z = 0; z < favColors.length; z++) {
+  console.log(z, favColors[z]);
+}
+const favNumbers = [42, 3543, 98, 81, 8, 10, 73, 10];
+for (let a = 0; a < favNumbers.length; a++) {
+  console.log(a, favNumbers[a]);
+}
+
+let startNumber = 0;
+while (startNumber <= 132) {
+  console.log(startNumber);
+  startNumber++;
+}
